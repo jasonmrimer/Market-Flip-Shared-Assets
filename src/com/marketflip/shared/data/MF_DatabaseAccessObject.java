d79d0e76d29a31b7e6ff3e2f5b12ec100f4e64a0
David Walters <dwalters423@gmail.com>
2015-10-17 18:12:43 +0100

    Stub created for DAO. Error loading MySQL driver, needs afixin'

--- src/com/marketflip/shared/data/MF_DatabaseAccessObject.java
+++ src/com/marketflip/shared/data/MF_DatabaseAccessObject.java
@@ -1,5 +1,27 @@
 package com.marketflip.shared.data;
 
-public class MF_DatabaseAccessObject {
+import java.sql.*;
 
+/*
+ * @author David Walters
+ * The library to access the products CloudSQL instance using google's Cloud SQL API.
+ * Updated - 10/17/2015
+ */
+
+public class MF_DatabaseAccessObject {
+	private static final String IP = ***REMOVED***;                         //The IP address of the products database
+	private static final String URL = "jdbc:google:mysql://marketflip-crawler"  //The URL of the database.
+			+ ":products/products?user=root";
+	private Connection connection;
+	
+	public MF_DatabaseAccessObject () {
+		try {
+			Class.forName("com.mysql.jdbc.Driver"); //TODO: Broken reference. Figure out how to load driver.
+			connection = DriverManager.getConnection(URL);
+		}
+		catch (Exception e) {
+			System.out.println("Error connecting to CloudSQL: ");
+			e.printStackTrace();
+		}
+	}
 }
---
+++ src/com/marketflip/shared/data/test/TestApp.java
@@ -0,0 +1,16 @@
+package com.marketflip.shared.data.test;
+
+import com.marketflip.shared.data.MF_DatabaseAccessObject;
+/**
+ * Test application to test the DAO during development.
+ * @author David Walters
+ *
+ */
+public class TestApp {
+
+	public static void main(String[] args) {
+		
+		MF_DatabaseAccessObject DAO = new MF_DatabaseAccessObject();
+	}
+
+}
