15e2519845e9606e7e44c1b4b39a53222cade6c6
David Walters <dwalters423@gmail.com>
2015-12-13 21:38:37 +0000

    Began DAO v2 and tests.

---
+++ src/com/marketflip/shared/data/MF_DataAccessObject.java
@@ -0,0 +1,125 @@
+package com.marketflip.shared.data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.HashSet;
+
+import com.marketflip.shared.data.exceptions.ProductValidationException;
+import com.marketflip.shared.products.MF_Product;
+
+public class MF_DataAccessObject {
+	
+	private static final String IP 			= ***REMOVED***;
+	private static final String URL 		= "jdbc:mysql://" + IP + ":3306/PRODUCTS";
+	private static final String USERNAME 	= ***REMOVED***;   //SHAME!! TODO: End the shame.
+	private static final String PASSWORD	= ***REMOVED***;       //SHAME!! TODO: End the shame.
+	
+	private static final int 	
+		INFO_COMPANY_INDEX  		= 1,
+		INFO_UPC_INDEX				= 2,
+		INFO_NAME_INDEX				= 3,
+		INFO_HEIGHT_INDEX			= 4,
+		INFO_WIDTH_INDEX			= 5,
+		INFO_LENGTH_INDEX			= 6,
+		INFO_DESCRIPTION_INDEX 		= 7,
+		INFO_URL_INDEX				= 8,
+		INFO_CURRENT_PRICE_INDEX	= 9;
+	
+	private static final int	
+		PRICE_DATE_INDEX			= 1,
+		PRICE_COMPANY_INDEX			= 2;
+	
+	private Connection 						connection;
+	private HashSet<String> 				productSet 	= new HashSet<String> ();
+	private static MF_DataAccessObject 		MF_DAO		= null;
+	
+	public static void openConnection() {
+		MF_DAO = new MF_DataAccessObject();
+	}
+	
+	public static MF_DataAccessObject getDAO () {
+		if (MF_DAO != null) {
+			return MF_DAO;
+		} else {
+			return null;
+		}
+	}
+	
+	private MF_DataAccessObject () {
+		try {
+			
+			String 		sql;
+			Statement 	sqlStatement;
+			ResultSet	rs;
+			
+			
+			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			
+			connection.setAutoCommit(false);
+			
+			sql = "SELECT UPC FROM PRODUCTS";
+			sqlStatement = connection.createStatement();
+			rs = sqlStatement.executeQuery(sql);
+			
+			while (rs.next()) {
+				String upc = rs.getString("UPC");
+				productSet.add(upc);
+			}
+			
+		}
+		catch (Exception e) {
+			System.err.println("Error connecting to CloudSQL: ");
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Attempts to close the Connection for this instance.
+	 * The MF_DatabaseAccessObject should always be closed when the program is done using
+	 * it.
+	 */
+	public void close() {
+		
+		try {
+			this.connection.close();
+			MF_DataAccessObject.MF_DAO = null;
+		} catch (Exception e) {
+			System.err.println ("Error closing connection: ");
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Insert a new Product into the database.
+	 * @param MF_Product Product to be inserted
+	 */
+	public boolean insertProduct(MF_Product product) {
+		
+		try {
+			validateProduct(product);
+		} catch (ProductValidationException e) {
+			System.err.println("ERROR: Product not inserted.");
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
+		
+	}
+	
+	private void validateProduct (MF_Product product) throws ProductValidationException {
+		
+		if (product == null) {
+			throw new ProductValidationException("Product cannot be null.");
+		}
+		if (product.getUPC() == "" || product.getUPC() == null) {
+			throw new ProductValidationException("UPC cannot be null.");
+		}
+		
+	}
+
+}
---
+++ src/com/marketflip/shared/data/test/MF_DataAccessObjectTest.java
@@ -0,0 +1,89 @@
+package com.marketflip.shared.data.test;
+
+
+/**
+ * Tests the DAO for all edge cases.
+ * @author David Walters
+ * Last updated 12/13/2015
+ */
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import com.marketflip.shared.data.*;
+import com.marketflip.shared.data.exceptions.ProductValidationException;
+import com.marketflip.shared.products.*;
+
+
+
+public class MF_DataAccessObjectTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		
+		MF_DataAccessObject.openConnection();
+		
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		
+		//Ensure the DAO is closed.
+		if (MF_DataAccessObject.getDAO() != null) {
+			MF_DataAccessObject.getDAO().close();
+		}
+	}
+	
+	@Test
+	public void getDAO_Singleton_ExpectNoExceptions () {
+		assertFalse (MF_DataAccessObject.getDAO() == null);
+	}
+	
+	@Test
+	public void Close_Singleton_ExpectNull () {
+		MF_DataAccessObject.getDAO().close();
+		assertTrue(MF_DataAccessObject.getDAO() == null);
+	}
+	
+	@Test
+	public void Insert_NullProduct_ExpectFalse () {
+		
+		MF_Product product = null;
+		assertFalse(MF_DataAccessObject.getDAO().insertProduct(product));	
+		
+	}
+	
+	@Test
+	public void Insert_NullUPC_ExpectFalse () {
+		
+		String upc = null;
+		MF_Product product = new MF_Product(upc);
+		assertFalse(MF_DataAccessObject.getDAO().insertProduct(product));	
+		
+	}
+	
+	
+}
